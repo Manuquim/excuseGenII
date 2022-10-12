1a1e67c86acbe91a5b6f94f9027b026c8783fa46
Manuquim <manuelcabreraosorio11@gmail.com>
2022-10-12 19:45:52 +0000

    excuseGenII

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
-/* eslint-disable */
-import "bootstrap";
+//* eslint-disable */
+
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector("#the-excuse").innerHTML = generarExcusa();
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generarExcusa();
+  });
+  /*console.log("Hola desde el espacio");*/
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let generarExcusa = () => {
+  let who = ["El perro", "Mi vecino", "La jirafa", "El jefe"];
+  let what = ["come", "canta", "pelea", "grita"];
+  let when = [
+    "antes de clase",
+    "en la plaza",
+    "cuando bosteza",
+    "en el almuerzo",
+    "cuando estoy corriendo"
+  ];
+  let whoRand = numRandom(who.length);
+  let whaRand = numRandom(what.length);
+  let wheRand = numRandom(when.length);
+  return who[whoRand] + " " + what[whaRand] + " " + when[wheRand];
+};
+let numRandom = max => {
+  /*No le pone parentesis a la variable de entrada*/
+  let n = Math.floor(Math.random() * 10);
+  while (n >= max) {
+    n = Math.floor(Math.random() * 10);
+  }
+  return n;
 };
